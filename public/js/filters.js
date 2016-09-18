8bfaad07eb0a1ed277995759255221369df4b2e7
Christie R <creindle@hawaii.edu>
2016-09-17 18:48:00 -1000

    Model bound filter works

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,6 +1,5 @@
 angular.module('myApp')
   .filter('beforeYearFilter', function () {
-    console.log(arguments);
     return function(movies, year) {
       return movies.filter(movie => movie.year < year);
     }
